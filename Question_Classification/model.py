a8bc1fd6b622afe8feaff948bc6c278739f4f499
manhph2211 <maxph22112000@gmail.com>
2021-03-16 15:11:58 +0700

    update

--- Question_Classification/model.py
+++ Question_Classification/model.py
@@ -29,12 +29,13 @@ class LSTM(nn.Module):
     def __init__(self, input_size, output_size, hidden_dim, n_layers):
         super(LSTMs, self).__init__()
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_dim, num_layers=n_layers, bidirectional=True)
+        self.relu = nn.ReLU()
         self.linear1 = nn.Linear(in_features=hidden_dim*2, out_features=output_size)
         self.classifier = nn.Softmax()
 
     def forward(self, X):
         out,hidden = self.lstm(X)
-        #print(out.size())
+        out = self.relu(out)
         out = out[:, -1, :]
         out = self.linear1(out)
         return out
